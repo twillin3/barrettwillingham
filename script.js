2f312aba0748ebd6fe06f7cda8c416fdde0aaccc
Barrett Willingham <115803980+twillin3@users.noreply.github.com>
2022-11-04 14:21:29 +0000

    Final hopefully

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// example function call to display a dialog box
-// let's comment out the code below, which is top-level code 
-// that automatically runs when the script is loaded
-//alert("welcome, this isn't is a javascript popup alert");
-
 function submitForm(name, hometown) {
-  // search the DOM for a specific tag with the id "guestlist"
+
   var guestlist = document.getElementById("guestlist");
   var n = document.getElementById("name");
   var h = document.getElementById("hometown");
@@ -12,5 +7,28 @@ function submitForm(name, hometown) {
   n.value = "";
   h.value = "";
   n.focus();
+  return false;
+}
+
+function clearStorage() {
+  var answerlist = document.getElementById("answerlist");
+  answerlist.innerHTML = "";
+  localStorage.setItem("answerlist","");
+}
+
+function checkStorage() {
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  answerlist.innerHTML = currentanswers;
+}
+
+function submitAnswer(answer) {
+
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  localStorage.setItem("answerlist", currentanswers + "<br />" + answer.value);
+  answerlist.innerHTML += "<br />" + answer.value;
+  answer.value = "";
+  answer.focus();
   return false;
 }
